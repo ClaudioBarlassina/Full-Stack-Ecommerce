f4356e269e5b1afd51e052f851cbb2d6f95e0b03
Claudio Barlassina <claudio_barlassina_ar@hotmail.com>
2026-04-16 00:07:26 -0300

    cambiourl

--- vite-project/FrontEnd/src/api/products.api.js
+++ vite-project/FrontEnd/src/api/products.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/productos"; // tu backend Mongo o JSON
+const API_URL = "https://full-stack-ecommerce-d1a4.onrender.com/productos"; // tu backend Mongo o JSON
 
 // Traer todos los productos, pasando filtros como query params
 export const getProducts = (filters = {}) => {
